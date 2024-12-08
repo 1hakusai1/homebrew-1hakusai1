641e15fbdd459599a6d9bbeac3ab1c821216ee6f
1hakusai1 <1hakusai1@gmail.com>
2024-12-08 23:15:10 +0900

    fix wrong hash

--- Formula/awscurl-rs.rb
+++ Formula/awscurl-rs.rb
@@ -2,7 +2,7 @@ class AwscurlRs < Formula
     desc "curl with AWS sigv4 signing written in Rust"
     homepage "https://github.com/1hakusai1/awscurl-rs"
     url "https://github.com/1hakusai1/awscurl-rs/releases/download/0.1.0/awscurlrs_0.1.0_darwin_arm64.zip"
-    sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
+    sha256 "8e8b33143c27c79ac81fe001434d0b33405af461d3e6d8294624747fe3e92570"
     version "0.1.0"
   
     def install
