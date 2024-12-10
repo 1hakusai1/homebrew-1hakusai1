7e89066ed1b179bce72f57bf521ffebde9407349
1hakusai1 <1hakusai1@gmail.com>
2024-12-10 12:28:05 +0900

    Release awscurl-rs 0.8.0

--- Formula/awscurl-rs.rb
+++ Formula/awscurl-rs.rb
@@ -1,9 +1,9 @@
 class AwscurlRs < Formula
     desc "curl with AWS sigv4 signing written in Rust"
     homepage "https://github.com/1hakusai1/awscurl-rs"
-    url "https://github.com/1hakusai1/awscurl-rs/releases/download/0.7.0/awscurl-rs_0.7.0_darwin_arm64.zip"
-    sha256 "48fa5501d75ff751ed52495fe8e219fc7ff7c6e7bf40b7ed8360e9d2f8e8591c"
-    version "0.7.0"
+    url "https://github.com/1hakusai1/awscurl-rs/releases/download/0.8.0/awscurl-rs_0.8.0_darwin_arm64.zip"
+    sha256 "724bb6d4fd8e068891891c7eb9edc8d0823a65aa054b87f44ba83de0d529fe7c"
+    version "0.8.0"
   
     def install
       bin.install "awscurl"
