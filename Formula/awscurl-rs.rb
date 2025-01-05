d4f67a589112d1e9d8fb71b4fb442d12fc3db9a1
1hakusai1 <1hakusai1@gmail.com>
2025-01-05 21:00:25 +0900

    Release awscurl-rs 0.12.0
    Add shell completions

--- Formula/awscurl-rs.rb
+++ Formula/awscurl-rs.rb
@@ -1,11 +1,12 @@
 class AwscurlRs < Formula
     desc "curl with AWS sigv4 signing written in Rust"
     homepage "https://github.com/1hakusai1/awscurl-rs"
-    url "https://github.com/1hakusai1/awscurl-rs/releases/download/0.11.0/awscurl-rs_0.11.0_darwin_arm64.zip"
-    sha256 "57c48458c2c9dcbf8eefcb3a2b19e89d162495229a75d23affb5ab7ac28d5521"
+    url "https://github.com/1hakusai1/awscurl-rs/releases/download/0.12.0/awscurl-rs_0.12.0_darwin_arm64.zip"
+    sha256 "1c797f19b5e28f0e3402146b8ed56265f6492dd15567568b3c7c95fc88afb73e"
     version "0.11.0"
   
     def install
       bin.install "awscurl"
+      generate_completions_from_executable(bin/"awscurl", "''", "--generate-shell-completion")
     end
   end
